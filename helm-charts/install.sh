bfd1e8f4d743d3130f2475477fd073f12213b7fb
Harrison (Hieu) Van <Hieu_Van@mckinsey.com>
2021-10-18 09:45:48 +0700

    feat: helm install

---
+++ helm-charts/install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -eoux pipefail
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# minikube start --driver=virtualbox --cpus=3 --memory=5000
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+kubectl delete namespace elastic-stack || true
+kubectl create namespace elastic-stack
+
+kubectl create secret generic elastic-credentials \
+  --from-literal=username=elastic \
+  --from-literal=password=Passw0rd \
+  --namespace=elastic-stack
+
+helm install elasticsearch ./helm-charts/elasticsearch \
+  --namespace=elastic-stack \
+  --values=helm-charts/values/elasticsearch.yaml
+
+helm install kibana ./helm-charts/kibana \
+  --namespace=elastic-stack \
+  --values=helm-charts/values/kibana.yaml
+
+set +x
+
+printf "\n"
+echo "kubectl port-forward --namespace=elastic-stack svc/elasticsearch-master 9200"
+echo "Access: http://localhost:9200"
+
+printf "\n"
+echo "kubectl port-forward --namespace=elastic-stack svc/kibana-kibana 5601"
+echo "Access: http://localhost:5601"
+
+printf "\n"
+echo "Welcome to Elastic"
+echo "Username: elastic"
+echo "Password: Passw0rd"
+
+printf "\n"
